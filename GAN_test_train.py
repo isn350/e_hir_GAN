405c499db99a71a899c30a3ce05d4f9f4294a5bd
isn350 <isn350@gmail.com>
2019-05-31 23:24:34 -0700

    Update GAN_test_train.py

--- GAN_test_train.py
+++ GAN_test_train.py
@@ -4,12 +4,11 @@ import time
 import tensorflow as tf
 def train_gan(activity):
 	logdir = '1'
-	data_path = 'C:/Users/STUDENT/Desktop/Ibrahim/GAN_tot/o/' + activity + '/train/'
-	outputdata = 'C:/Users/STUDENT/Desktop/Ibrahim/GAN_tot/o/'
+	data_path = 'C:/Users/STUDENT/Desktop/Ibrahim/GAN_tot/o/' + activity + '/train/' # directly to plot training data for visualization 
+	outputdata = 'C:/Users/STUDENT/Desktop/Ibrahim/GAN_tot/o/' # directly to visualization GAN output 
 	gan_input,_  = read_data(data_path,[[activity]])
 	train(logdir,64,gan_input,outputdata,activity)
 
 if __name__ == '__main__':
-	#time.sleep(1800)	
 	activity = 'boxingmoving'
 	train_gan(activity)
